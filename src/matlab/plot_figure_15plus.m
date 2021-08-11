6bb3fd59b6cb51e055e699f671fe8450e151c33f
Elias Jarlebring <jarlebring@gmail.com>
2021-08-11 09:35:35 +0200

    matlab loading of degopt

---
+++ src/matlab/plot_figure_15plus.m
@@ -0,0 +1,17 @@
+degopt1=read_degopt('/home/jarl/jobb/src/GraphMatFunData/data/exp/exp_sastre_m4.cgr',true);
+degopt2=read_degopt('/home/jarl/jobb/src/GraphMatFunData/data/exp/exp_sastre_m4_opt_rho0_69.cgr',true);
+
+xv=0:0.05:1.0;
+xv(1) = xv(2)/10;
+err1=vpa(zeros(size(xv)));
+err2=vpa(zeros(size(xv)));
+
+for i=1:length(xv)
+    x=vpa(xv(i));
+    err1(i)=(eval_degopt(degopt1,x)-exp(x))/exp(x);
+    err2(i)=(eval_degopt(degopt2,x)-exp(x))/exp(x);
+end
+
+semilogy(xv,double(abs(err1)),xv,double(abs(err2)));
+
+legend('15+', 'opt');
